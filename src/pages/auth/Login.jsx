d894704793d539f423fd021a5487c1d8dba4428a
codesbytobii <olanrewaju.oluwatobi@ffsdgroup.com>
2025-02-07 13:46:14 +0100

    .

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,11 +1,4 @@
-import {
-  Button,
-  Row,
-  Col,
-  FormGroup,
-  FormLabel,
-  FormControl,
-} from "react-bootstrap";
+import { Button, Row, Col, FormGroup, FormLabel, FormControl } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import classNames from "classnames";
@@ -48,46 +41,30 @@ const BottomLink = () => {
 
 /* social links */
 const SocialLinks = () => {
-  const socialLinks = [
-    {
-      variant: "primary",
-      icon: "facebook",
-    },
-    {
-      variant: "danger",
-      icon: "google",
-    },
-    {
-      variant: "info",
-      icon: "twitter",
-    },
-    {
-      variant: "secondary",
-      icon: "github",
-    },
-  ];
-  return (
-    <>
-      <ul className="social-list list-inline mt-3 mb-0">
-        {(socialLinks || []).map((item, index) => {
-          return (
-            <li key={index} className="list-inline-item">
-              <Link
-                to="#"
-                className={classNames(
-                  "social-list-item",
-                  "border-" + item.variant,
-                  "text-" + item.variant
-                )}
-              >
-                <i className={classNames("mdi", "mdi-" + item.icon)}></i>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </>
-  );
+  const socialLinks = [{
+    variant: "primary",
+    icon: "facebook"
+  }, {
+    variant: "danger",
+    icon: "google"
+  }, {
+    variant: "info",
+    icon: "twitter"
+  }, {
+    variant: "secondary",
+    icon: "github"
+  }];
+  return <>
+            <ul className="social-list list-inline mt-3 mb-0">
+                {(socialLinks || []).map((item, index) => {
+        return <li key={index} className="list-inline-item">
+                            <Link to="#" className={classNames("social-list-item", "border-" + item.variant, "text-" + item.variant)}>
+                                <i className={classNames("mdi", "mdi-" + item.icon)}></i>
+                            </Link>
+                        </li>;
+      })}
+            </ul>
+        </>;
 };
 const Login = () => {
 
@@ -106,88 +83,49 @@ const Login = () => {
   
 
   const [showPassword, setShowPassword] = useState(false);
-  return (
-    <>
-      <AuthLayout
-        helpText={t(
-          "Enter your email address and password to access admin panel."
-        )}
-        bottomLinks={<BottomLink />}
-      >
-        <form onSubmit={login}>
-          <div className="mb-3">
-            <Controller
-              name="email"
-              control={control}
-              render={({ field, fieldState }) => (
-                <FormGroup>
-                  <FormLabel htmlFor="email">{t("Email")}</FormLabel>
-                  <FormControl
-                    id="email"
-                    {...field}
-                    isInvalid={Boolean(fieldState.error?.message)}
-                  />
-                  {fieldState.error?.message && (
-                    <Feedback type="invalid" className="text-danger">
-                      {fieldState.error?.message}
-                    </Feedback>
-                  )}
-                </FormGroup>
-              )}
-            />
-          </div>
+  return <>
+            <AuthLayout helpText={t("Enter your email address and password to access admin panel.")} bottomLinks={<BottomLink />}>
 
-          <div className="mb-3">
-            <Controller
-              name="password"
-              control={control}
-              render={({ field, fieldState }) => (
-                <FormGroup>
-                  <FormLabel htmlFor="password">{t("Password")}</FormLabel>
+                <form onSubmit={login}>
 
-                  <div className="position-relative">
-                    <FormControl
-                      id="password"
-                      type={showPassword ? "text" : "password"}
-                      {...field}
-                      isInvalid={Boolean(fieldState.error?.message)}
-                    />
-                    {fieldState.error?.message && (
-                      <Feedback type="invalid" className="text-danger">
-                        {fieldState.error?.message}
-                      </Feedback>
-                    )}
-                    <span
-                      className="d-flex position-absolute top-50 end-0 translate-middle-y p-0 pe-2 me-2"
-                      onClick={() => setShowPassword(!showPassword)}
-                    >
-                      {!fieldState.error &&
-                        (showPassword ? (
-                          <FiEye
-                            height={18}
-                            width={18}
-                            className="cursor-pointer"
-                          />
-                        ) : (
-                          <FiEyeOff
-                            height={18}
-                            width={18}
-                            className="cursor-pointer"
-                          />
-                        ))}
-                    </span>
-                  </div>
-                </FormGroup>
-              )}
-            />
-          </div>
+                    <div className="mb-3">
+                        <Controller name="email" control={control} render={({
+            field,
+            fieldState
+          }) => <FormGroup>
+                                    <FormLabel htmlFor="email">
+                                        {t("Email")}
+                                    </FormLabel>
+                                    <FormControl id="email" {...field} isInvalid={Boolean(fieldState.error?.message)} />
+                                    {fieldState.error?.message && <Feedback type="invalid" className="text-danger">{fieldState.error?.message}</Feedback>}
+                                </FormGroup>} />
+                    </div>
 
-          <div className="text-center d-grid">
-            <Button variant="primary" type="submit">
-              {t("Log In")}
-            </Button>
-          </div>
-        </form>
+                    <div className="mb-3">
+                        <Controller name="password" control={control} render={({
+            field,
+            fieldState
+          }) => <FormGroup>
+                                    <FormLabel htmlFor="password">
+                                        {t("Password")}
+                                    </FormLabel>
+
+                                    <div className="position-relative">
+                                        <FormControl id="password" type={showPassword ? 'text' : 'password'} {...field} isInvalid={Boolean(fieldState.error?.message)} />
+                                        {fieldState.error?.message && <Feedback type="invalid" className="text-danger">{fieldState.error?.message}</Feedback>}
+                                        <span className="d-flex position-absolute top-50 end-0 translate-middle-y p-0 pe-2 me-2" onClick={() => setShowPassword(!showPassword)}>
+                                      {!fieldState.error && (showPassword ? <FiEye height={18} width={18} className="cursor-pointer" /> : <FiEyeOff height={18} width={18} className="cursor-pointer" />)}
+                                    </span>
+                                    </div>
+                                </FormGroup>} />
+                    </div>
+
+                    <div className="text-center d-grid">
+                        <Button variant="primary" type="submit">
+                            {t("Log In")}
+                        </Button>
+                    </div>
+                </form>
 
                 {/* SOCIAL LINK */}
                 {/* <div className="text-center">
